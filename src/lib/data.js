88f75f9ebc2e37a61ae4d59e6b3ce9b2c9502b58
Alejandro Meza <alemz-li@outlook.com>
2023-10-08 15:06:10 -0700

    Initial commit

---
+++ src/lib/data.js
@@ -0,0 +1,57 @@
+export const skills = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "REST API",
+  "Node.js",
+  "Git",
+  "Express",
+  "Multer",
+  "React",
+  "React Query",
+  "Tailwindcss",
+  "MongoDB",
+  "Mongoose",
+  "Prisma",
+  "Parcel",
+  "Astro",
+];
+
+export const experience = [
+  {
+    period: "2019 - 2023",
+    role: "Programmer/Backend Coordinator @ Grupo Concordia",
+    description:
+      "First joined this Real Estate company as a programmer maintaining a system with Php and CodeIgniter framework, then successfully migrated intricate business logic into a robust CRM platform called Zoho. Implementing solutions and applications with HTML, CSS, Javascript and REST API.",
+  },
+];
+
+export const projects = [
+  {
+    name: "Lydia: Code Bites",
+    description: "Platform for saving and sharing your code snippets",
+    tech: [
+      "React",
+      "React Query",
+      "Node.js",
+      "MongoDB",
+      "Express",
+      "Tailwindcss",
+    ],
+    repo: "https://github.com/alemz-li/lydia",
+    demo: null,
+  },
+  {
+    name: "Personal Movie Collection Site",
+    description: "Website showcasing movies I own",
+    tech: ["React", "React Query", "Node.js", "MongoDB", "Express"],
+    repo: "https://github.com/alemz-li/movie-collection",
+    demo: "https://alemz-collection.onrender.com/",
+  },
+  {
+    name: "Find Flick",
+    description: "Get a random movie suggestion",
+    tech: ["React", "Node.js", "Express", "CSS", "REST API"],
+    demo: "https://find-flick.onrender.com/",
+  },
+];
